82500d089c40650b9ab0a1f492b427f24dc2c620
Airuikeric <Huajianchen666@gmail.com>
2024-11-12 23:44:05 -0600

    Learning JavaScript - Full couirse for Beginners

---
+++ script.js
@@ -0,0 +1,35 @@
+// comment
+var number = 5; // this is a in-line comment
+
+/* this is a 
+... ...
+... ...
+    ... .   .
+    multi-line cvomment
+*/
+number = 9;
+
+/* Data Types:
+undefined - is something that hasn't been defined, a varibale that you haven't set to be anythign yet
+null - nothing
+boolean - true or false
+string
+symbol - symbol is an ummutable primitive valoue that is unique
+number - is number 
+object - can store a lot of different key value pairs
+*/
+
+// There are three way to declare a varible
+// 1. var stand for variable and here is how do you declare a varible
+var myName = "Eric"
+myName = 8
+
+// 2. let is the second way to declare a varible
+// let will only be used within the scope of where you declared that 
+let ourName = "freeCodeCamp"
+
+// 3. const pi = 3.14 is the thrid way to declare a varible
+// const is a variable that should never change
+const pi = 3.14
+
+// 
